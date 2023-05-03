251a38337f6f45c44916f45d35726abef148f278
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-05-03 20:40:36 +0400

    Update Conan packages

--- conanfile.py
+++ conanfile.py
@@ -3,15 +3,15 @@ from conan import ConanFile
 class ConanCache(ConanFile):
     settings = "arch", "build_type", "compiler", "os"
     requires = (
-        "imgui/1.87",
+        "imgui/1.89.4",
         "spdlog/1.11.0",
-        "bullet3/3.24",
+        "bullet3/3.25",
         "lodepng/cci.20200615",
         "zstd/1.5.0"
     )
 
     def requirements(self):
         if self.settings.os in ["Windows", "Linux"]:
-            self.requires("glfw/3.3.5")
+            self.requires("glfw/3.3.8")
         if self.settings.os in ["Windows", "Linux", "Macos"]:
             self.requires("gtest/1.13.0")
