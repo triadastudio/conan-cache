d1b5627eb2bad93dfacbea18b42e0cf13f673226
SenPie <smbat@duck.com>
2024-09-21 01:31:17 +0200

    Update Conan dependencies
    ImGui: 1.90.0 -> 1.91.0
    spdlog: 1.12.0 -> 1.14.1
    GLFW: 3.3.8 -> 3.4

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conan import ConanFile
 class ConanCache(ConanFile):
     settings = "arch", "build_type", "compiler", "os"
     requires = (
-        "imgui/1.90",
-        "spdlog/1.12.0",
+        "imgui/1.91.0",
+        "spdlog/1.14.1",
         "bullet3/3.25",
         "lodepng/cci.20200615",
         "zstd/1.5.5"
@@ -12,6 +12,6 @@ class ConanCache(ConanFile):
 
     def requirements(self):
         if self.settings.os in ["Windows", "Linux"]:
-            self.requires("glfw/3.3.8")
+            self.requires("glfw/3.4")
         if self.settings.os in ["Windows", "Linux", "Macos"]:
             self.requires("gtest/1.13.0")
